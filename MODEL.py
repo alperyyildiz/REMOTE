05ec8ecc3f1e1adfc062b962f39950106e8a1d93
alperyyildiz <50810809+alperyyildiz@users.noreply.github.com>
2020-04-04 04:00:37 +0300

    Update MODEL.py

--- MODEL.py
+++ MODEL.py
@@ -1,3 +1,31 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import pickle
+import time
+
+from torch import optim
+from sklearn import datasets
+from torch.autograd import Variable
+from torch.utils.data import DataLoader
+from torch.utils.data import TensorDataset
+from sklearn.preprocessing import StandardScaler
+from statsmodels.tsa.seasonal import seasonal_decompose, STL, DecomposeResult
+from sklearn.model_selection import GridSearchCV
+from hyperopt import fmin, tpe, hp, STATUS_OK, base,Trials
+cuda = torch.device('cuda')
+
+print('IMPORTS ARE DONE!!!')
+
+def my_imports(module_name):
+    globals()[module_name] = __import__(module_name)
+
+
+
+
 class Model(nn.Module, PARAMETERS):
     def __init__(self, LIST, OTHERS, SCLR, TRAIN, VAL):
         super().__init__()
