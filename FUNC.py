dea4fc19ec859729f0835ec7474f1d480b78b858
alperyyildiz <50810809+alperyyildiz@users.noreply.github.com>
2020-04-04 04:01:18 +0300

    Update FUNC.py

--- FUNC.py
+++ FUNC.py
@@ -1,3 +1,30 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import pickle
+import time
+
+from torch import optim
+from sklearn import datasets
+from torch.autograd import Variable
+from torch.utils.data import DataLoader
+from torch.utils.data import TensorDataset
+from sklearn.preprocessing import StandardScaler
+from statsmodels.tsa.seasonal import seasonal_decompose, STL, DecomposeResult
+from sklearn.model_selection import GridSearchCV
+from hyperopt import fmin, tpe, hp, STATUS_OK, base,Trials
+cuda = torch.device('cuda')
+
+print('IMPORTS ARE DONE!!!')
+
+def my_imports(module_name):
+    globals()[module_name] = __import__(module_name)
+
+
+
 def fit(epochs, model, opt, train_dl, valid_dl):
     min_loss = 10
     for epoch in range(epochs):
